470591f28f0c7a65f6c9a79d11fa7ede4b80688e
Dane Olsen <danekolsen@gmail.com>
2020-05-27 18:37:37 -0700

    Continued work on modals

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,193 +1,193 @@
-let projects = [
-  {
-    title: "Unlawful Detainer (UD) Talk!",
-    description:
-      "Build a chatbot that helps tenants answer the Unlawful Dentainer form (the UD-105) and generate a PDF. This can help Self Represented Litigants answer the UD-105 while queuing, helping the Self Help Legal Center staff to serve more people. This could be useful to triage the client's needs in advance, should the team at the SHLC need to tackle specific issues with someone.",
-    lead: "Juanjo",
-    status: "Active",
-    skills: ["Developer", "UI/UX", "Web accessibility"],
-    techStack: ["SMS", "landline.io", "to be determined"],
-    slackChannel: "https://codeforsanjose.slack.com/messages/ud-talk/",
-  },
-  {
-    title: "OSM South Bay Mapping",
-    description:
-      "Import building data to Open Street Map, an open source map that anyone can edit. There are many applications, such as emergency disaster response and mapping navigation",
-    lead: "Minh Nguyen",
-    status: "Active",
-    skills: [
-      "People who want to learn mapping",
-      "data entry",
-      "mappers",
-      "community organizers",
-      "project leaders",
-      "government GIS personnel",
-    ],
-    techStack: ["OpenStreetMap"],
-    githubUrl: "https://github.com/codeforsanjose/OSM-SouthBay",
-    slackChannel: "https://codeforsanjose.slack.com/messages/osm/",
-  },
-  {
-    title: "Open Disclosure San José",
-    description:
-      "Build a web application to inform San José residents about local campaign finance through data visualization",
-    lead: "Helen Skinner",
-    status: "Active",
-    skills: [
-      "Data analyst",
-      "Front end developer",
-      "Back end developer",
-      "Communication",
-      "Policy analysis",
-      "Web accessibility",
-      "UI designers",
-      "UX researchers",
-    ],
-    techStack: ["Python", "JavaScript", "CSS"],
-    githubUrl: "https://github.com/codeforsanjose/open-disclosure",
-    slackChannel: "https://codeforsanjose.slack.com/messages/open-disclosure/",
-  },
-  {
-    title: "Heart of the Valley",
-    description:
-      "Map of public art and murals in the south bay of Bay Area, California",
-    lead: "John Lau",
-    status: "Active",
-    skills: [
-      "Mappers",
-      "Front end developer",
-      "Backend developer",
-      "Web accessibility",
-      "UI designers",
-      "UX researchers",
-    ],
-    techStack: ["JavaScript", "Mapbox GL JS", "Bootstrap", "HTML", "CSS"],
-    liveUrl: "https://www.codeforsanjose.com/heartofthevalley/",
-    githubUrl: "https://github.com/codeforsanjose/heartofthevalley",
-    slackChannel:
-      "https://codeforsanjose.slack.com/messages/heart-of-the-valley/",
-  },
-  {
-    title: "San José Public Library Meal Tally App",
-    description:
-      "Web app to help San José public libraries coordinate free community meals. The web app is live in production and used by 6+ libraries. Current goal is to refactor the website and improve security.",
-    lead: "Joseph Richardson",
-    status: "Active",
-    skills: [
-      "DevOps",
-      "Cybersecurity",
-      "Front end developer",
-      "Back end developer",
-      "UI Designer",
-      "UX research",
-      "Communication",
-      "Government/policy staff",
-    ],
-    techStack: ["MongoDB", "React", "JavaScript", "CSS"],
-    githubUrl: "https://github.com/codeforsanjose/SJPL-MealTally/",
-    slackChannel:
-      "https://codeforsanjose.slack.com/messages/sj-public-library/",
-  },
-  {
-    title: "Census 2020",
-    description:
-      "Create an informational website for the City of San José to inform San José residents why the Census 2020 matters and encourage residents to participate in the Census.",
-    lead: "Anathea Lolen",
-    status: "Active",
-    skills: [
-      "Web accessibility",
-      "Language translation",
-      "Web performance",
-      "Front end developer",
-      "UX researcher",
-    ],
-    techStack: ["OpenStreetMap"],
-    liveUrl: "https://census-2020.herokuapp.com/",
-    githubUrl: "https://github.com/codeforsanjose/census2020",
-    slackChannel: "https://codeforsanjose.slack.com/messages/census2020/",
-  },
-  {
-    title: "Santa Clara County Probation Department Website",
-    description:
-      "Redesign probation department website to help inform youth and families about resources and improve user experience.",
-    lead: "Cesar Arellano",
-    status: "Active",
-    skills: [
-      "Front end developers",
-      "CMS",
-      "UI designer",
-      "Web accessibility",
-      "UX research",
-      "Government/policy staff",
-      "Content writer/editor",
-      "Marketing",
-    ],
-    techStack: ["Wix"],
-    slackChannel: "https://codeforsanjose.slack.com/messages/scc-probation/",
-  },
-  {
-    title: "Disaster Response",
-    description:
-      "Disaster Response SJ is a React based web application that notifies people of active disaster information for San José and includes disaster planning resources, plus an administrative interface for disaster response managers to create/maintain active disaster updates.",
-    lead: "Joseph Richardson",
-    status: "Active",
-    skills: [
-      "Subject matter experts",
-      "Web Developers",
-      "GIS analysts",
-      "mappers",
-      "data analysts",
-      "UI designers",
-      "UX researchers",
-      "database managers",
-    ],
-    techStack: ["React Native", "JavaScript"],
-    githubUrl: "https://github.com/codeforsanjose/disaster-response-sj",
-    slackChannel:
-      "https://codeforsanjose.slack.com/messages/disaster-response-sj/",
-  },
-  {
-    title: "Code for San José website",
-    description:
-      "Design and build website, identify bugs, fix issues, and maintain content on website",
-    lead: "Dane Olsen",
-    status: "Active",
-    skills: [
-      "Front end developer",
-      "Marketing",
-      "Designer",
-      "Content writer",
-      "UX Researcher",
-    ],
-    techStack: ["HTML", "CSS", "jQuery", "JavaScript", "Bootstrap"],
-    githubUrl: "https://github.com/codeforsanjose/codeforsanjose.github.io",
-    slackChannel: "https://codeforsanjose.slack.com/messages/cfsj-website/",
-    liveUrl: "https://www.codeforsanjose.com/",
-  },
-  {
-    title: "Razing the Bar",
-    description:
-      "Redesign website for nonprofit Razing the Bar. The project was completed by September 2019.",
-    lead: "Sunny Mui",
-    status: "Complete",
-    skills: ["UX research", "UI design", "CMS", "Wordpress"],
-    techStack: ["Wordpress"],
-    liveUrl: "https://www.razingthebar.org/",
-  },
-  {
-    title: "Map of Silicon Valley development projects with Catalyze SV",
-    description:
-      "An interactive map highlighting development projects for a local non-profit, Catalyze SV.",
-    lead: "Lena Tran",
-    status: "Complete",
-    skills: ["Front end developer", "UI design", "CMS", "User researcher"],
-    techStack: ["Mapbox GL JS", "Wix", "JavaScript"],
-    liveUrl: "https://www.catalyzesv.org/map",
-    githubUrl:
-      "https://github.com/codeforsanjose/Map-of-Silicon-Valley-development-projects",
-    slackChannel: "https://codeforsanjose.slack.com/messages/catalyze-sv-map",
-  },
-];
+// let projects = [
+//   {
+//     title: "Unlawful Detainer (UD) Talk!",
+//     description:
+//       "Build a chatbot that helps tenants answer the Unlawful Dentainer form (the UD-105) and generate a PDF. This can help Self Represented Litigants answer the UD-105 while queuing, helping the Self Help Legal Center staff to serve more people. This could be useful to triage the client's needs in advance, should the team at the SHLC need to tackle specific issues with someone.",
+//     lead: "Juanjo",
+//     status: "Active",
+//     skills: ["Developer", "UI/UX", "Web accessibility"],
+//     techStack: ["SMS", "landline.io", "to be determined"],
+//     slackChannel: "https://codeforsanjose.slack.com/messages/ud-talk/",
+//   },
+//   {
+//     title: "OSM South Bay Mapping",
+//     description:
+//       "Import building data to Open Street Map, an open source map that anyone can edit. There are many applications, such as emergency disaster response and mapping navigation",
+//     lead: "Minh Nguyen",
+//     status: "Active",
+//     skills: [
+//       "People who want to learn mapping",
+//       "data entry",
+//       "mappers",
+//       "community organizers",
+//       "project leaders",
+//       "government GIS personnel",
+//     ],
+//     techStack: ["OpenStreetMap"],
+//     githubUrl: "https://github.com/codeforsanjose/OSM-SouthBay",
+//     slackChannel: "https://codeforsanjose.slack.com/messages/osm/",
+//   },
+//   {
+//     title: "Open Disclosure San José",
+//     description:
+//       "Build a web application to inform San José residents about local campaign finance through data visualization",
+//     lead: "Helen Skinner",
+//     status: "Active",
+//     skills: [
+//       "Data analyst",
+//       "Front end developer",
+//       "Back end developer",
+//       "Communication",
+//       "Policy analysis",
+//       "Web accessibility",
+//       "UI designers",
+//       "UX researchers",
+//     ],
+//     techStack: ["Python", "JavaScript", "CSS"],
+//     githubUrl: "https://github.com/codeforsanjose/open-disclosure",
+//     slackChannel: "https://codeforsanjose.slack.com/messages/open-disclosure/",
+//   },
+//   {
+//     title: "Heart of the Valley",
+//     description:
+//       "Map of public art and murals in the south bay of Bay Area, California",
+//     lead: "John Lau",
+//     status: "Active",
+//     skills: [
+//       "Mappers",
+//       "Front end developer",
+//       "Backend developer",
+//       "Web accessibility",
+//       "UI designers",
+//       "UX researchers",
+//     ],
+//     techStack: ["JavaScript", "Mapbox GL JS", "Bootstrap", "HTML", "CSS"],
+//     liveUrl: "https://www.codeforsanjose.com/heartofthevalley/",
+//     githubUrl: "https://github.com/codeforsanjose/heartofthevalley",
+//     slackChannel:
+//       "https://codeforsanjose.slack.com/messages/heart-of-the-valley/",
+//   },
+//   {
+//     title: "San José Public Library Meal Tally App",
+//     description:
+//       "Web app to help San José public libraries coordinate free community meals. The web app is live in production and used by 6+ libraries. Current goal is to refactor the website and improve security.",
+//     lead: "Joseph Richardson",
+//     status: "Active",
+//     skills: [
+//       "DevOps",
+//       "Cybersecurity",
+//       "Front end developer",
+//       "Back end developer",
+//       "UI Designer",
+//       "UX research",
+//       "Communication",
+//       "Government/policy staff",
+//     ],
+//     techStack: ["MongoDB", "React", "JavaScript", "CSS"],
+//     githubUrl: "https://github.com/codeforsanjose/SJPL-MealTally/",
+//     slackChannel:
+//       "https://codeforsanjose.slack.com/messages/sj-public-library/",
+//   },
+//   {
+//     title: "Census 2020",
+//     description:
+//       "Create an informational website for the City of San José to inform San José residents why the Census 2020 matters and encourage residents to participate in the Census.",
+//     lead: "Anathea Lolen",
+//     status: "Active",
+//     skills: [
+//       "Web accessibility",
+//       "Language translation",
+//       "Web performance",
+//       "Front end developer",
+//       "UX researcher",
+//     ],
+//     techStack: ["OpenStreetMap"],
+//     liveUrl: "https://census-2020.herokuapp.com/",
+//     githubUrl: "https://github.com/codeforsanjose/census2020",
+//     slackChannel: "https://codeforsanjose.slack.com/messages/census2020/",
+//   },
+//   {
+//     title: "Santa Clara County Probation Department Website",
+//     description:
+//       "Redesign probation department website to help inform youth and families about resources and improve user experience.",
+//     lead: "Cesar Arellano",
+//     status: "Active",
+//     skills: [
+//       "Front end developers",
+//       "CMS",
+//       "UI designer",
+//       "Web accessibility",
+//       "UX research",
+//       "Government/policy staff",
+//       "Content writer/editor",
+//       "Marketing",
+//     ],
+//     techStack: ["Wix"],
+//     slackChannel: "https://codeforsanjose.slack.com/messages/scc-probation/",
+//   },
+//   {
+//     title: "Disaster Response",
+//     description:
+//       "Disaster Response SJ is a React based web application that notifies people of active disaster information for San José and includes disaster planning resources, plus an administrative interface for disaster response managers to create/maintain active disaster updates.",
+//     lead: "Joseph Richardson",
+//     status: "Active",
+//     skills: [
+//       "Subject matter experts",
+//       "Web Developers",
+//       "GIS analysts",
+//       "mappers",
+//       "data analysts",
+//       "UI designers",
+//       "UX researchers",
+//       "database managers",
+//     ],
+//     techStack: ["React Native", "JavaScript"],
+//     githubUrl: "https://github.com/codeforsanjose/disaster-response-sj",
+//     slackChannel:
+//       "https://codeforsanjose.slack.com/messages/disaster-response-sj/",
+//   },
+//   {
+//     title: "Code for San José website",
+//     description:
+//       "Design and build website, identify bugs, fix issues, and maintain content on website",
+//     lead: "Dane Olsen",
+//     status: "Active",
+//     skills: [
+//       "Front end developer",
+//       "Marketing",
+//       "Designer",
+//       "Content writer",
+//       "UX Researcher",
+//     ],
+//     techStack: ["HTML", "CSS", "jQuery", "JavaScript", "Bootstrap"],
+//     githubUrl: "https://github.com/codeforsanjose/codeforsanjose.github.io",
+//     slackChannel: "https://codeforsanjose.slack.com/messages/cfsj-website/",
+//     liveUrl: "https://www.codeforsanjose.com/",
+//   },
+//   {
+//     title: "Razing the Bar",
+//     description:
+//       "Redesign website for nonprofit Razing the Bar. The project was completed by September 2019.",
+//     lead: "Sunny Mui",
+//     status: "Complete",
+//     skills: ["UX research", "UI design", "CMS", "Wordpress"],
+//     techStack: ["Wordpress"],
+//     liveUrl: "https://www.razingthebar.org/",
+//   },
+//   {
+//     title: "Map of Silicon Valley development projects with Catalyze SV",
+//     description:
+//       "An interactive map highlighting development projects for a local non-profit, Catalyze SV.",
+//     lead: "Lena Tran",
+//     status: "Complete",
+//     skills: ["Front end developer", "UI design", "CMS", "User researcher"],
+//     techStack: ["Mapbox GL JS", "Wix", "JavaScript"],
+//     liveUrl: "https://www.catalyzesv.org/map",
+//     githubUrl:
+//       "https://github.com/codeforsanjose/Map-of-Silicon-Valley-development-projects",
+//     slackChannel: "https://codeforsanjose.slack.com/messages/catalyze-sv-map",
+//   },
+// ];
 
 var isMenuOpen = false;
 
--- assets/js/project-info.js
+++ assets/js/project-info.js
@@ -1,16 +1,70 @@
-function openModal(id) {
-  console.log(id);
+var public_spreadsheet_url =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3vCQ3cY0xP5oEIc6-B3N23q8hhbKEzDyfvRH_neOMZA3hBS_zU2RkjRCTYrpTjcliINqUuY_G0dsE/pub?gid=684908702&single=true&output=csv";
+
+let projects = [];
+
+function init() {
+  Papa.parse(public_spreadsheet_url, {
+    download: true,
+    header: true,
+    complete: showInfo,
+  });
+}
+
+window.addEventListener("DOMContentLoaded", init);
+
+function showInfo(results) {
+  projects = results.data;
+  let workDiv = document.getElementById("modal-attach");
+  workDiv.innerHTML = results.data.map((project) => {
+    console.log(project);
+    return `<div class="project-card">
+            <p class="badge ${project.status}">${project.status}</p>
+            <img class="project-img" src="../assets${project.photo1}" alt="">
+            <h4>${project.title}</h4>
+            <p>${project.project_summary}</p>
+            <a href="${project.github_url}" target="_blank" rel="noref noopener">
+            <img src="./assets/img/social_logos/GitHub-Mark-Light-120px-plus.png" alt="Github" class="project-gh-logo"></a>
+            <a href='${project.live_url}' target="_blank" rel="noref noopener"><i class="fas fa-external-link-alt"></i></a>
+            <p class="tech">Tech: ${project.tech}</p>
+            <button onclick="openModal('${project.title}')" class="learn-more">Learn More</button>
+          </div>`;
+  });
+}
+
+function openModal(title) {
+  document.addEventListener("keydown", accessibleModalClose);
+  const pagePos = window.scrollY;
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${pagePos}px`;
+  var modalProject = projects.filter((item) => item.title.includes(title));
+  let project = modalProject[0];
+  console.log(project);
   let projectModal = document.createElement("div");
   projectModal.setAttribute("class", "projectModalStyle");
   projectModal.setAttribute("id", "openModal");
   projectModal.innerHTML = `
-    <button onclick="closeModal()">X</button>
-    <h1>Project Title: ${id}</h1>
-  `;
+      <i onclick="closeModal()" class="fas fa-2x fa-times"></i>
+      <h2>${project.title}</h2>
+      <img src='../assets${project.photo2}'>
+      <img src='../assets${project.photo3}'>
+      <p>${project.project_description}</p>
+    `;
   document.getElementById("modal-attach").appendChild(projectModal);
 }
 
+function accessibleModalClose(e) {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+}
+
 function closeModal() {
+  document.removeEventListener("keydown", accessibleModalClose);
+  const scrollY = document.body.style.top;
+  document.body.style.position = "";
+  document.body.style.top = "";
+  window.scrollTo(0, parseInt(scrollY || "0") * -1);
   modal = document.getElementById("openModal");
   document.getElementById("modal-attach").removeChild(modal);
 }
