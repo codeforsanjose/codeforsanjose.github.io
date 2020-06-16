f27a64aec0557c1d406908cad1cb3bd328366835
Dane Olsen <danekolsen@gmail.com>
2020-06-16 16:09:44 -0700

    Finished initial layout for responsive modals, and duplicated function for project page. Changed public art images for banners and added alt text pull from db

--- assets/js/project-info-projects.js
+++ assets/js/project-info-projects.js
@@ -16,20 +16,24 @@ window.addEventListener("DOMContentLoaded", init);
 function showInfo(results) {
   projects = results.data;
   let workDiv = document.getElementById("modal-attach");
-  workDiv.innerHTML = results.data.map((project) => {
-    console.log(project);
-    return `<div class="project-card">
+  workDiv.innerHTML = results.data
+    .map((project) => {
+      console.log(project);
+      return `<div class="project-card">
             <p class="badge ${project.status}">${project.status}</p>
-            <img class="project-img" src="../assets${project.photo1}" alt="">
+            <img onerror='this.style.display = "none"' class="project-img" src="../${project.photo1}" alt="${project.photo1_alt}" />
             <h4>${project.title}</h4>
             <p>${project.project_summary}</p>
+            <div id="project-links">
             <a href="${project.github_url}" target="_blank" rel="noref noopener">
-            <img src="./assets/img/social_logos/GitHub-Mark-Light-120px-plus.png" alt="Github" class="project-gh-logo"></a>
-            <a href='${project.live_url}' target="_blank" rel="noref noopener"><i class="fas fa-external-link-alt"></i></a>
+            <img src="../img/social_logos/GitHub-Mark-Light-120px-plus.png" alt="Github" class="project-gh-logo"></a>
+            <a href='${project.live_url}' target="_blank" rel="noref noopener"><i class="fas fa-2x fa-external-link-alt"></i></a>
+            </div>
             <p class="tech">Tech: ${project.tech}</p>
             <button onclick="openModal('${project.title}')" class="learn-more">Learn More</button>
           </div>`;
-  });
+    })
+    .join("");
 }
 
 function openModal(title) {
@@ -45,10 +49,16 @@ function openModal(title) {
   projectModal.setAttribute("id", "openModal");
   projectModal.innerHTML = `
       <i onclick="closeModal()" class="fas fa-2x fa-times"></i>
-      <h2>${project.title}</h2>
-      <img src='../assets${project.photo2}'>
-      <img src='../assets${project.photo3}'>
-      <p>${project.project_description}</p>
+      <div class="modal-imgs">
+        <img onerror='this.style.display = "none"' src='../${project.photo2}' alt="${project.photo2_alt}">
+        <img onerror='this.style.display = "none"' src='../${project.photo3}' alt="${project.photo3_alt}">
+      </div>
+      <div class="modal-info">
+        <h2>${project.title}</h2>
+        <p>${project.project_description}</p>
+        <p>Skills needed: ${project.skills}</p>
+        <p>Partner(s): ${project.partner}<p>
+      </div>
     `;
   document.getElementById("modal-attach").appendChild(projectModal);
 }
