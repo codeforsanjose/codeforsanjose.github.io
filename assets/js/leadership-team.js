20c046845789f252b2c636cb2d09e9e0f686c013
Yan-Yin Choy <yanyinchoy@gmail.com>
2020-11-01 20:26:30 -0800

    add new leadership structure and organize data about alumni

--- assets/js/leadership-team.js
+++ assets/js/leadership-team.js
@@ -1,19 +1,14 @@
 const leaders = [
   {
-    captains: [
+    executiveDirectors: [
       {
         name: "Yan-Yin Choy",
-        image: "./assets/img/leadership/captains/yan-yin.png",
+        image: "./assets/img/leadership/exec_directors/yan-yin.png",
         link: "http://linkedin.com/in/yanyinchoy",
       },
       {
-        name: "Joey Richardson",
-        image: "./assets/img/leadership/captains/joey.png",
-        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
-      },
-      {
         name: "Annie Steenson",
-        image: "./assets/img/leadership/captains/annie.png",
+        image: "./assets/img/leadership/exec_directors/annie.png",
         link: "http://linkedin.com/in/anniesteenson",
       },
     ],
@@ -21,73 +16,179 @@ const leaders = [
   {
     leader: [
       {
-        name: "Cesar Arellano",
-        image: "./assets/img/leadership/team/cesar.png",
-        link: "https://www.linkedin.com/in/cesaarel",
+        name: "Dane Olsen",
+        image: "./assets/img/leadership/team/dane.png",
+        title: "Director of Member Engagement",
+        link: "https://www.linkedin.com/in/danekolsen/",
+      },
+      {
+        name: "Darren Pham",
+        image: "./assets/img/leadership/team/darren.jpg",
+        title: "Director of Backend Engineering",
+        link: "https://www.linkedin.com/in/darep-h20/",
       },
       {
         name: "Diane Labenz",
         image: "./assets/img/leadership/team/diane.png",
+        title: "Director of Design & Usability",
         link: "https://www.linkedin.com/in/dianelabenz/",
       },
       {
-        name: "Minh Nguyễn",
-        image: "./assets/img/leadership/team/minh.jpg",
-        link: "http://notes.1ec5.org/",
+        name: "Diana Laster",
+        image: "./assets/img/leadership/team/diana.jpeg",
+        title: "Director of Project Management",
+        link: "https://www.linkedin.com/in/dianalaster/",
       },
       {
-        name: "Dane Olsen",
-        image: "./assets/img/leadership/team/dane.png",
-        link: "https://www.linkedin.com/in/danekolsen/",
+        name: "Hattie Chau",
+        image: "./assets/img/leadership/team/hattie.jpeg",
+        title: "Marketing Committee Member",
+        link: "https://www.linkedin.com/in/hattie-chau/",
+      },
+      {
+        name: "Joey Richardson",
+        image: "./assets/img/leadership/team/joey.png",
+        title: "Director of Frontend Engineering",
+        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
+      },
+      {
+        name: "Karen Li",
+        image: "./assets/img/leadership/team/karen.jpeg",
+        title: "Assistant Director of Design & Usability",
+        link: "https://www.linkedin.com/in/li-0431/",
       },
       {
         name: "Lena Tran",
         image: "./assets/img/leadership/team/lena.png",
+        title: "Director of Data Analytics",
         link: "https://www.linkedin.com/in/lenatran4/",
       },
       {
-        name: "Kate Valdes",
-        image: "./assets/img/leadership/team/kate.png",
-        link: "https://katevaldes.com/portfolio/",
+        name: "Namrata Challa",
+        image: "./assets/img/leadership/team/namrata.jpg",
+        title: "Marketing Committee Member",
+        link: "https://www.linkedin.com/in/namratachalla/",
+      },
+      {
+        name: "Ryan Walek",
+        title: "Director of Fundraising",
+        image: "./assets/img/leadership/team/ryan.jpg",
+        link: "https://www.linkedin.com/in/ryanwalek",
+      },
+      {
+        name: "Trace Ohrt",
+        image: "./assets/img/leadership/team/trace.jpeg",
+        title: "Assistant Director of Backend Engineering",
+        link: "https://www.linkedin.com/in/trace-ohrt/",
+      }
+    ],
+  },
+  {
+    projectLeaders: [
+      {
+        name: "Alessandro Pianetta",
+        image: "./assets/img/leadership/project_leaders/alex.jpeg",
+        title: "Open Disclosure San José",
+        link: "https://www.linkedin.com/in/apianetta/",
+      },
+      {
+        name: "Annie Steenson",
+        image: "./assets/img/leadership/exec_directors/annie.png",
+        title: "City Agenda Notifier",
+        link: "http://linkedin.com/in/anniesteenson",
+      },
+      {
+        name: "Ashwin Wagadarikar",
+        image: "./assets/img/leadership/project_leaders/ashwin.jpeg",
+        title: "BayAreaCommunity.org",
+        link: "https://www.linkedin.com/in/ashwinwagadarikar//",
+      },
+      {
+        name: "Cesar Arellano",
+        image: "./assets/img/leadership/former_leadership/cesar.png",
+        title: "Virtual Services for Inmates",
+        link: "https://www.linkedin.com/in/cesaarel",
+      },
+      {
+        name: "Darren Pham",
+        image: "./assets/img/leadership/team/darren.jpg",
+        title: "Open Disclosure San José",
+        link: "https://www.linkedin.com/in/darep-h20/",
+      },
+      {
+        name: "Geraldine Eleazer",
+        image: "./assets/img/leadership/project_leaders/geraldine.jpeg",
+        title: "211 Chat Bot for Sonoma County",
+        link: "https://www.linkedin.com/in/geraldine-eleazar-b98b4a123/",
+      },
+      {
+        name: "Joey Richardson",
+        image: "./assets/img/leadership/team/joey.png",
+        title: "E-Immigrate",
+        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
+      },
+      {
+        name: "Mark Noack",
+        image: "./assets/img/leadership/project_leaders/mark.jpeg",
+        title: "City Agenda Scraper",
+        link: "https://www.linkedin.com/in/mark-noack-90b7682b/",
+      },
+      {
+        name: "Minh Nguyễn",
+        image: "./assets/img/leadership/former_leadership/minh.jpg",
+        title: "OpenStreetMap (OSM)",
+        link: "http://notes.1ec5.org/",
+      },
+      {
+        name: "Ryan Walek",
+        title: "San José Civic User Testing (CUT) Group",
+        image: "./assets/img/leadership/team/ryan.jpg",
+        link: "https://www.linkedin.com/in/ryanwalek",
+      },
+      {
+        name: "Yan-Yin Choy",
+        image: "./assets/img/leadership/exec_directors/yan-yin.png",
+        title: "City Agenda Notifier, SMS helpline for Tenants, Heart of the Valley",
+        link: "http://linkedin.com/in/yanyinchoy",
       },
     ],
   },
   {
-    advisory: [
+    formerLeadership: [
       {
-        name: "Lorin Camargo",
-        image: "./assets/img/leadership/advisory/lorin.jpg",
-        link: "https://www.linkedin.com/in/lorincamargo/",
+        name: "Cesar Arellano",
+        image: "./assets/img/leadership/former_leadership/cesar.png",
+        link: "https://www.linkedin.com/in/cesaarel",
       },
       {
-        name: "Namrata Challa",
-        image: "./assets/img/leadership/advisory/namrata.jpg",
-        link: "https://www.linkedin.com/in/namratachalla/",
+        name: "Minh Nguyễn",
+        image: "./assets/img/leadership/former_leadership/minh.jpg",
+        link: "http://notes.1ec5.org/",
       },
       {
         name: "Erika Hudiono",
-        image: "./assets/img/leadership/advisory/erika.jpg",
+        image: "./assets/img/leadership/former_leadership/erika.jpg",
         link: "https://www.linkedin.com/in/erika-hudiono/",
       },
       {
         name: "Sunny Mui",
-        image: "./assets/img/leadership/advisory/sunny.jpg",
+        image: "./assets/img/leadership/former_leadership/sunny.jpg",
         link: "https://www.linkedin.com/in/sunnymui/",
       },
       {
-        name: "Darren Pham",
-        image: "./assets/img/leadership/advisory/darren.jpg",
-        link: "https://www.linkedin.com/in/darep-h20/",
-      },
-      {
         name: "Emily Ramos",
-        image: "./assets/img/leadership/advisory/emily.jpg",
+        image: "./assets/img/leadership/former_leadership/emily.jpg",
         link: "https://www.linkedin.com/in/emilyannramos/",
       },
       {
-        name: "Michelle Thong",
-        image: "./assets/img/leadership/advisory/michelle.jpg",
-        link: "https://www.linkedin.com/in/mthong/",
+        name: "Lorin Camargo",
+        image: "./assets/img/leadership/former_leadership/lorin.jpg",
+        link: "https://www.linkedin.com/in/lorincamargo/",
+      },
+      {
+        name: "Kate Valdes",
+        image: "./assets/img/leadership/former_leadership/kate.png",
+        link: "https://katevaldes.com/portfolio/",
       },
     ],
   },
@@ -105,27 +206,36 @@ const leaders = [
       },
       {
         name: "Michelle Thong",
-        image: "./assets/img/leadership/advisory/michelle.jpg",
+        image: "./assets/img/leadership/former_captains/michelle.jpg",
         link: "https://www.linkedin.com/in/mthong/",
+      },
+      {
+        name: "Joey Richardson",
+        image: "./assets/img/leadership/team/joey.png",
+        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
       },
     ],
   },
 ];
 
-document.getElementById("captains").innerHTML = leaders[0].captains
-  .map((captain) => {
+document.getElementById("executive-directors").innerHTML = leaders[0].executiveDirectors
+  .map((executiveDirector) => {
     return `<div class="leader captain">
-        <a href=${captain.link} target="_blank" rel="noopener noref">
+        <a href=${executiveDirector.link} target="_blank" rel="noopener noref">
           <img src=${
-            captain.image
-              ? captain.image
+            executiveDirector.image
+              ? executiveDirector.image
               : "./assets/img/leadership/default.png"
-          } alt=${captain.name} />
-          <p>${captain.name}</p>
+          } alt=${executiveDirector.name} />
+          <p>${executiveDirector.name}</p>
         </a>
       </div>`;
   })
   .join("");
+
+// TODO: add leader title if it exists
+// If you add that logic, need to also update 
+// styles so it can accommodate a newline for leader title
 
 for (var i = 1; i < leaders.length; i++) {
   let label = Object.keys(leaders[i]);
