746e8eb09668c993064c9eef8f43c63d76914483
Joseph Richardson <jrichardson@jmstudios.net>
2023-12-06 16:24:06 -0800

    updating details

--- assets/js/leadership-team.js
+++ assets/js/leadership-team.js
@@ -1,47 +1,34 @@
+const Joey = {
+  name: "Joey Richardson",
+  image: "./assets/img/leadership/team/joey.png",
+  title: "Co-Captain",
+  link: "https://www.linkedin.com/in/joseph-richardson-artisan-engineer/",
+};
+const Darren = {
+  name: "Darren Pham",
+  image: "./assets/img/leadership/team/darren.jpg",
+  title: "Director of Backend Engineering",
+  link: "https://www.linkedin.com/in/darep-h20/",
+};
+const Ellina = {
+  name: "Ellina Yin",
+  image: "./assets/img/leadership/team/joey.png",
+  title: "Co-Captain",
+  link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
+};
+const Minh = {
+  name: "Minh Nguyễn",
+  image: "./assets/img/leadership/former_leadership/minh.jpg",
+  title: "OpenStreetMap (OSM)",
+  link: "http://notes.1ec5.org/",
+};
+
 const leaders = [
   {
-    executiveDirectors: [
-      {
-        name: "Joey Richardson",
-        image: "./assets/img/leadership/team/joey.png",
-        title: "Interim Captain",
-        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
-      }
-    ]
+    executiveDirectors: [Joey, Ellina],
   },
   {
-    leader: [
-      {
-        name: "Joey Richardson",
-        image: "./assets/img/leadership/team/joey.png",
-        title: "Director of Frontend Engineering",
-        link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
-      },
-      {
-        name: "Darren Pham",
-        image: "./assets/img/leadership/team/darren.jpg",
-        title: "Director of Backend Engineering",
-        link: "https://www.linkedin.com/in/darep-h20/",
-      },
-      {
-        name: "Diana Laster",
-        image: "./assets/img/leadership/team/diana.jpeg",
-        title: "Director of Project Management",
-        link: "https://www.linkedin.com/in/dianalaster/",
-      },
-      {
-        name: "Erika Hudiono",
-        image: "./assets/img/leadership/former_leadership/erika.jpg",
-        title: "Director of Operations",
-        link: "https://www.linkedin.com/in/erika-hudiono/",
-      },
-      {
-        name: "Diane Labenz",
-        image: "./assets/img/leadership/team/diane.png",
-        title: "Director of Design & Usability",
-        link: "https://www.linkedin.com/in/dianelabenz/",
-      }
-    ],
+    leader: [Joey, Ellina, Darren, Minh],
   },
   {
     projectLeaders: [
@@ -52,65 +39,16 @@ const leaders = [
         link: "http://notes.1ec5.org/",
       },
       {
-        name: "Alessandro Pianetta",
-        image: "./assets/img/leadership/project_leaders/alex.jpeg",
-        title: "Open Disclosure San José",
-        link: "https://www.linkedin.com/in/apianetta/",
-      },
-      {
-        name: "Annie Steenson",
-        image: "./assets/img/leadership/exec_directors/annie.png",
-        title: "City Agenda Notifier",
-        link: "http://linkedin.com/in/anniesteenson",
-      },
-      {
-        name: "Ashwin Wagadarikar",
-        image: "./assets/img/leadership/project_leaders/ashwin.jpeg",
-        title: "BayAreaCommunity.org",
-        link: "https://www.linkedin.com/in/ashwinwagadarikar//",
-      },
-      {
-        name: "Cesar Arellano",
-        image: "./assets/img/leadership/former_leadership/cesar.png",
-        title: "Virtual Services for Inmates",
-        link: "https://www.linkedin.com/in/cesaarel",
-      },
-      {
         name: "Darren Pham",
         image: "./assets/img/leadership/team/darren.jpg",
         title: "Open Disclosure San José",
         link: "https://www.linkedin.com/in/darep-h20/",
       },
       {
-        name: "Geraldine Eleazer",
-        image: "./assets/img/leadership/project_leaders/geraldine.jpeg",
-        title: "211 Chat Bot for Sonoma County",
-        link: "https://www.linkedin.com/in/geraldine-eleazar-b98b4a123/",
-      },
-      {
         name: "Joey Richardson",
         image: "./assets/img/leadership/team/joey.png",
-        title: "E-Immigrate",
+        title: "E-Immigrate, Happening ATM, Heart of the Valley",
         link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
-      },
-      {
-        name: "Mark Noack",
-        image: "./assets/img/leadership/project_leaders/mark.jpeg",
-        title: "City Agenda Scraper",
-        link: "https://www.linkedin.com/in/mark-noack-90b7682b/",
-      },
-
-      {
-        name: "Ryan Walek",
-        title: "San José Civic User Testing (CUT) Group",
-        image: "./assets/img/leadership/team/ryan.jpg",
-        link: "https://www.linkedin.com/in/ryanwalek",
-      },
-      {
-        name: "Yan-Yin Choy",
-        image: "./assets/img/leadership/exec_directors/yan-yin.png",
-        title: "City Agenda Notifier, SMS helpline for Tenants, Heart of the Valley",
-        link: "http://linkedin.com/in/yanyinchoy",
       },
     ],
   },
@@ -126,7 +64,6 @@ const leaders = [
         image: "./assets/img/leadership/former_leadership/minh.jpg",
         link: "http://notes.1ec5.org/",
       },
-
       {
         name: "Sunny Mui",
         image: "./assets/img/leadership/former_leadership/sunny.jpg",
@@ -165,9 +102,9 @@ const leaders = [
         name: "Michelle Thong",
         image: "./assets/img/leadership/former_captains/michelle.jpg",
         link: "https://www.linkedin.com/in/mthong/",
-      }
+      },
       // {
-      //   name: "Joey Richardson",
+      //   name: "Yan Yin Choy",
       //   image: "./assets/img/leadership/team/joey.png",
       //   link: "https://www.linkedin.com/in/joseph-richardson-97206953/",
       // },
@@ -175,9 +112,10 @@ const leaders = [
   },
 ];
 
-document.getElementById("executive-directors").innerHTML = leaders[0].executiveDirectors
-  .map((executiveDirector) => {
-    return `<div class="leader captain">
+document.getElementById("executive-directors").innerHTML =
+  leaders[0].executiveDirectors
+    .map((executiveDirector) => {
+      return `<div class="leader captain">
         <a href=${executiveDirector.link} target="_blank" rel="noopener noref">
           <img src=${
             executiveDirector.image
@@ -187,13 +125,12 @@ document.getElementById("executive-directors").innerHTML = leaders[0].executiveD
           <p>${executiveDirector.name}</p>
         </a>
       </div>`;
-  })
-  .join("");
+    })
+    .join("");
 
 // TODO: add leader title if it exists
-// If you add that logic, need to also update 
+// If you add that logic, need to also update
 // styles so it can accommodate a newline for leader title
-
 
 for (var i = 1; i < leaders.length; i++) {
   let label = Object.keys(leaders[i]);
