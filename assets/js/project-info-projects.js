9d3801425ac1616130abb582636e3fe8d308b8fc
Minh Nguyễn <mxn@1ec5.org>
2025-04-30 21:07:23 -0700

    Reimplement project gallery as static content (#119)
    A new include loads the projects CSV file as a data file and constructs
    the project gallery all at build time, replacing JavaScript code that
    built the HTML at runtime. This eliminates the dependency on the
    PapaParse library.
    
    The modal works again. The modal’s content is now baked into the static
    webpage as a series of HTML data attributes on the card’s container
    element. The button that opens the modal passes the DOM element to the
    code that populates the modal.
    
    Deleted the mostly redundant script for populating the homepage version
    of the project gallery. Instead, the include accepts optional parameters
    to filter the gallery and/or limit to a maximum number of results.
    
    Updated the website’s own project card to mention the technologies and
    skills this project uses.
    
    Before | After
    ----|----
    ![14 projects, including maintained and archived projects, and an
    undefined
    project](https://github.com/user-attachments/assets/e2034088-f9e8-4952-a30d-e779bacc2766)
    | ![6 projects, all
    active](https://github.com/user-attachments/assets/1dfca8d3-9bbd-4c8b-8ce3-edad2766c41b)
    ![14 projects, including an undefined
    project](https://github.com/user-attachments/assets/e84d96ef-ebb9-48a1-a6e4-5c203eea910d)
    | ![13
    projects](https://github.com/user-attachments/assets/6867385e-6fe8-450f-941f-3fae6043192e)
    ![The whole page shifted to the
    left](https://github.com/user-attachments/assets/7deaf502-44ff-413f-bd23-fe32174c6782)
    | ![A modal with more details about the OSM
    project](https://github.com/user-attachments/assets/948d350d-e88c-41e8-98bb-7e5f88d322ae)
    
    Fixes #112 and fixes #118.

--- assets/js/project-info-projects.js
+++ assets/js/project-info-projects.js
@@ -1,60 +1,22 @@
-let projects = [];
-
-function init() {
-  Papa.parse("./assets/js/project_info.csv", {
-    download: true,
-    header: true,
-    complete: showInfo,
-  });
-}
-
-window.addEventListener("DOMContentLoaded", init);
-
-function showInfo(results) {
-  projects = results.data;
-  let workDiv = document.getElementById("modal-attach");
-  workDiv.innerHTML = results.data
-    .map((project) => {
-      console.log(project);
-      return `<div class="project-card">
-            <p class="badge ${project.status}">${project.status}</p>
-            <img onerror='this.style.display = "none"' class="project-img" src="../assets${project.photo1}" alt="${project.photo1_alt}" />
-            <h4>${project.title}</h4>
-            <p>${project.project_summary}</p>
-            <div id="project-links">
-            <a href="${project.github_url}" target="_blank" rel="noref noopener">
-            <img src="./assets/img/social_logos/GitHub-Mark-Light-120px-plus.png" alt="Github" class="project-gh-logo"></a>
-            <a href='${project.live_url}' target="_blank" rel="noref noopener"><i class="fas fa-2x fa-external-link-alt"></i></a>
-            </div>
-            <p class="tech">Tech: ${project.tech}</p>
-            <button onclick="openModal('${project.title}')" class="learn-more">Learn More</button>
-          </div>`;
-    })
-    .join("");
-}
-
-function openModal(title) {
+function openModal(card) {
   document.addEventListener("keydown", accessibleModalClose);
   const pagePos = window.scrollY;
   document.body.style.position = "fixed";
   document.body.style.top = `-${pagePos}px`;
-  var modalProject = projects.filter((item) => item.title.includes(title));
-  let project = modalProject[0];
-  console.log(project);
   let projectModal = document.createElement("div");
   projectModal.setAttribute("class", "projectModalStyle");
   projectModal.setAttribute("id", "openModal");
   projectModal.innerHTML = `
       <i onclick="closeModal()" class="fas fa-2x fa-times"></i>
       <div class="modal-imgs">
-        <img onerror='this.style.display = "none"' src='../assets${project.photo2}' alt="${project.photo2_alt}">
-        <img onerror='this.style.display = "none"' src='../assets${project.photo3}' alt="${project.photo3_alt}">
+        <img onerror='this.style.display = "none"' src='../assets${card.dataset.photo2}' alt="${card.dataset.photo2Alt}">
+        <img onerror='this.style.display = "none"' src='../assets${card.dataset.photo3}' alt="${card.dataset.photo3Alt}">
       </div>
       <div class="modal-info">
-        <h2>${project.title}</h2>
-        <p>${project.project_description}</p>
-        <p>Skills needed: ${project.skills}</p>
-        <p>Partner(s): ${project.partner}<p>
+        <h2>${card.dataset.title}</h2>
+        <p>${card.dataset.description}</p>
+        <p>Skills needed: ${card.dataset.skills}</p>
+        <p>Partner(s): ${card.dataset.partner}<p>
       </div>
     `;
   document.getElementById("modal-attach").appendChild(projectModal);
